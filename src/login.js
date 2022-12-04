0c2d6dfce6899d0eccecf454cf32ca29a48e7c4b
chris-mwesigwa <cmwesigwa@aetotechs.com>
2022-12-04 11:03:16 +0300

    first commit

---
+++ src/login.js
@@ -0,0 +1,14 @@
+const {username, password} = user
+
+
+
+
+function login(username, password){
+
+    return (<div className="App">
+    <header className="App-header">
+      
+    </header>
+</div>);
+
+}
